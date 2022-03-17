e5fff270d522d0918cb43a3389599a37062ed6e7
Daniel <virusxd521@gmail.com>
2022-03-17 18:27:07 +0200

    finalized the advertisements migration table

---
+++ database/migrations/2022_03_17_162055_create_advertisements_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('advertisements', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('position_id');
+            $table->foreignId('language_id');
+            $table->foreignId('game_id');
+            $table->foreignId('organization_id');
+            $table->foreignId('individual_id');
+            $table->foreignId('statuses_id');
+            $table->foreignId('ranks_id');
+            $table->foreignId('game_role_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('advertisements');
+    }
+};

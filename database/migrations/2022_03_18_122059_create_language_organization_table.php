ba82bf0380de376d26be0df17f51d733ac2d7634
Daniel <virusxd521@gmail.com>
2022-03-23 22:32:06 +0200

    Added changes from martin and filip and added s in migrations

--- database/migrations/2022_03_18_122059_create_language_organization_table.php
+++ database/migrations/2022_03_18_122059_create_language_organization_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('language_organization');
+        Schema::dropIfExists('language_organizations');
     }
 };

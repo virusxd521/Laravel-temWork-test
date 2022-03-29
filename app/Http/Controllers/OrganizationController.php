4b111ce27fa913a4754d2ea58bdfb1d326e45547
Martin <trnky76@hotmail.com>
2022-03-29 09:55:03 +0200

    comments to organizaitonController included

--- app/Http/Controllers/OrganizationController.php
+++ app/Http/Controllers/OrganizationController.php
@@ -32,41 +32,47 @@ class OrganizationController extends Controller
     public function data_for_advertisement_organization()
     {
 
-    //     $rank = new Rank;
-    //     //dd($rank->all());
+        //     $rank = new Rank;
+        //     //dd($rank->all());
 
-    //   $organizations = Organization::with(
-    //         [
-    //             'game',
-    //             'language',
-    //             'user',
-    //             'contact',
-    //             'advertisement',
-    //             'position',
-    //             'status',
-    //             'rank',
-    //             'role'
-    //         ]
-    //       )->get();
+        //   $organizations = Organization::with(
+        //         [
+        //             'game',
+        //             'language',
+        //             'user',
+        //             'contact',
+        //             'advertisement',
+        //             'position',
+        //             'status',
+        //             'rank',
+        //             'role'
+        //         ]
+        //       )->get();
 
-    $organizations = DB::table('organizations')
-        ->join('advertisements', 'organizations.id','=','advertisements.organization_id')
-        ->join('games', 'games.id','=','advertisements.game_id')
-        ->join('game_roles', 'game_roles.id','=','advertisements.game_role_id')
-        ->join('ranks', 'ranks.id','=','advertisements.rank_id')
-        ->join('positions', 'positions.id','=','advertisements.position_id')
-        ->join('advertisement_language', 'advertisement_language.advertisement_id','=','advertisements.id')
-        ->join('languages', 'advertisement_language.language_id','=','languages.id')  
-        ->select('organizations.id as id', 'organizations.name as name', 'positions.name as position', 'games.name as game', 'ranks.name as rank', 'game_roles.name as role', 'languages.name as language', 'advertisements.expectation as expectation', 'advertisements.offer as offer', 'advertisements.availability as availability')
-        ->get();
+        //query providing a complex information to displayed on organization card
+        //table contains every unique combination from advertisements table and will need to be processed
+        //into an array so that it can be JSONized
+        $organizations = DB::table('organizations')
+            ->join('advertisements', 'organizations.id','=','advertisements.organization_id')
+            ->join('games', 'games.id','=','advertisements.game_id')
+            ->join('game_roles', 'game_roles.id','=','advertisements.game_role_id')
+            ->join('ranks', 'ranks.id','=','advertisements.rank_id')
+            ->join('positions', 'positions.id','=','advertisements.position_id')
+            ->join('advertisement_language', 'advertisement_language.advertisement_id','=','advertisements.id')
+            ->join('languages', 'advertisement_language.language_id','=','languages.id')  
+            ->select('organizations.id as id', 'organizations.name as name', 'positions.name as position', 'games.name as game', 'ranks.name as rank', 'game_roles.name as role', 'languages.name as language', 'advertisements.expectation as expectation', 'advertisements.offer as offer', 'advertisements.availability as availability')
+            ->get();
 
-      //dd($organizations[0]->id);
-      //dd($organizations);
+        //dd($organizations[0]->id);
+        //dd($organizations);
 
+        //desired structure of output an associated array with single org ID and NAME and all other "properties" values to be conatined in an array (multiple languages, positions... from adverts) 
+        //declaration of dummy array processedOrgs to determin which oorganizaiton IDs hasve been inserted already
         $processedOrgs = [];
         $organizationsForJson = [];
         $currentOrgIndex = -1;
 
+        //looping through every single row of the output table from query
         foreach($organizations as $organization) {
          
             //dd($organization);
@@ -74,11 +80,13 @@ class OrganizationController extends Controller
             //       var_dump( $key." => ".$value.'\n');
             //   }
 
-            
+            //if processed or doesn't contain org ID it will be injected into organizaitonsForJson array with all the "properties"
             if (!in_array($organization->id, $processedOrgs)){
                 $processedOrgs[] = $organization->id;
                 $dummyArray = [];
 
+                //take all columns from the a single row of output, if they are ID or NAME inject it with values into dummy associated array,
+                //for the rest declare empty field as value for respective key
                 foreach($organization as $key=>$value) {
                     if($key == "id" || $key == "name"){
                         $dummyArray[$key] = $value;
@@ -88,6 +96,7 @@ class OrganizationController extends Controller
 
                 }
 
+                    //insert the dummy array into final array
                     $organizationsForJson[] = $dummyArray;
                     //dd($organizationsForJson);
                 
@@ -107,6 +116,7 @@ class OrganizationController extends Controller
             }
             //dd($organizationsForJson);
 
+            // for each row of the query output inject respective values into the empty arrays under respective key of the prepared final array
             foreach($organization as $key=>$value){
 
                 if($key == "id" || $key == "name") {
@@ -125,7 +135,7 @@ class OrganizationController extends Controller
         
             // if(!in_array($organizationsForJson))
         }
-
+        //dd($organizationsForJson);
         return $organizationsForJson;
     }
 }

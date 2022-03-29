41ee22529bf99a85b989bc2752fc855d9a566c12
Martin <trnky76@hotmail.com>
2022-03-29 11:52:30 +0200

    just redefined the length in statuses and organizations migration tables

--- database/migrations/2022_03_17_165629_create_organizations_table.php
+++ database/migrations/2022_03_17_165629_create_organizations_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->foreignId('user_id');
-            $table->string('name', 30);
+            $table->string('name', 50);
         });
     }
 
